d0fedca0838569c03e773061b8564f0adf9e5bb0
jonathanoliveirasilva9 <jonathanoliveirasilva9@gmail.com>
2023-08-06 23:40:32 -0300

    setting react-native-paper

--- src/features/Focus.js
+++ src/features/Focus.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { View, Text, StyleSheet } from 'react-native';
 import { colors } from '../utils/colors';
+import { TextInput } from "react-native-paper";
 
 export const Focus = () => (
   <View style={styles.container}>
-    <Text style={styles.text}>Focus Feature</Text>
+    <View style={styles.inputContainer}>
+      <TextInput label={'What would you like to focus on?'}></TextInput>
+    </View>
   </View>
 )
 
@@ -12,8 +15,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1
   },
+  inputContainer: {
+    flex: 1,
+    padding: 50,
+    justifyContent: 'center'
+  },
   text:{
     color: colors.white,
-    padding: 10
   }
 })
