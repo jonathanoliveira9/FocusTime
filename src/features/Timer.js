71342d8545e1f8d9a76577c20993f317f3116bb2
jonathanoliveirasilva9 <jonathanoliveirasilva9@gmail.com>
2023-09-10 00:40:00 -0300

    adding useKeepAwake

--- src/features/Timer.js
+++ src/features/Timer.js
@@ -3,6 +3,7 @@ import { View, StyleSheet, Text, Vibration } from 'react-native'
 import { ProgressBar } from 'react-native-paper';
 import { Countdown } from '../components/Countdown';
 import { RoundedButton } from '../components/RoundedButton';
+import { useKeepAwake } from 'expo-keep-awake';
 import { Timing } from './Timing';
 import { spacing } from '../utils/sizes';
 import { colors } from '../utils/colors';
@@ -18,6 +19,7 @@ const PATTERN = [
 ];
 
 export const Timer = ({ focusSubject, clearSubject }) => {
+  useKeepAwake();
   const [isStarted, setIsStarted] = useState(false);
   const [progress, setProgress] = useState(1);
   const [minutes, setMinutes] = useState(0.1);
