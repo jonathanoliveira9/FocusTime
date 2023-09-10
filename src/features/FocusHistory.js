63c82c36e3baba4503ab5eaf5ba2bbb024e3fc84
jonathanoliveirasilva9 <jonathanoliveirasilva9@gmail.com>
2023-09-10 01:01:33 -0300

    valid if exist history to show

--- src/features/FocusHistory.js
+++ src/features/FocusHistory.js
@@ -5,7 +5,7 @@ import { fontSizes, spacing } from '../utils/sizes';
 
 export const FocusHistory = ({ history }) => {
 
-  //if(!history || !history.length) return null;
+  if(!history || !history.length) return null;
 
   const renderItem = ({item}) => <Text style={styles.item}>- {item}</Text>
   return (
